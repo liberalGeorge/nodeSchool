a9b7c3ff59a63ef35a89891fb2abf93bf90c0c38
Georgina Hughes <me@georginahughes.com>
2015-11-04 21:48:03 +0000

    08 Completed

---
+++ learnYouNode/08HttpConnect.js
@@ -0,0 +1,19 @@
+/**
+ * Created by georginahughes on 01/11/15.
+ */
+var url = process.argv[2],
+    http = require('http'),
+    bl = require('bl');
+
+http.get(url, httpGetCallback);
+
+function httpGetCallback(response){
+    response.pipe(bl(blCallback));
+
+    function blCallback(err, data){
+        if(err) return;
+
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }
+}
